0d7cf06f6a18f232fb166e9eaddc5c53f3c59d2b
Giuseppetm <gdeambo@yahoo.it>
2021-08-20 22:44:57 +0200

    Update TotemTest.java

--- progetto/Bike Sharing/src/test/TotemTest.java
+++ progetto/Bike Sharing/src/test/TotemTest.java
@@ -1,14 +1,62 @@
 package test;
 
-import java.time.LocalDate;
+import java.util.NoSuchElementException;
 
 import org.junit.Test;
 import org.junit.jupiter.api.Assertions;
 
-import dominio.Abbonamento;
-import dominio.CartaDiCredito;
-import dominio.TipoAbbonamento;
+import dominio.Bicicletta;
+import dominio.Morsa;
+import dominio.TipoBicicletta;
+import dominio.TipoMorsa;
+import dominio.Totem;
 
 public class TotemTest {
+	/* Questo test verifica che non vengano sollevate eccezioni nel caso si noleggi una bicicletta che si sa essere disponibile */
+	@Test
+	public void noleggioBiciclettaTest() {
+		Totem totem = new Totem("Via Dennissonis 66");
+		Bicicletta bici = new Bicicletta(TipoBicicletta.ELETTRICA);
+        Morsa morsa = new Morsa(TipoMorsa.ELETTRICA);
+        morsa.chiudi(bici);
+        totem.aggiungiMorsa(morsa);
+        Assertions.assertDoesNotThrow(() -> { totem.noleggiaBicicletta(TipoBicicletta.ELETTRICA); });
+	}
 	
+	/* Questo test verifica che venga sollevata un'eccezione NoSuchElementException nel caso si tenti di noleggiare un tipo di bicicletta non disponibile nella postazione */
+	@Test
+	public void noleggioBiciclettaNonDisponibileTest() {
+		Totem totem = new Totem("Via Dennissonis 66");
+        Morsa morsa = new Morsa(TipoMorsa.ELETTRICA);
+        totem.aggiungiMorsa(morsa);
+        Assertions.assertThrows(NoSuchElementException.class, () -> { totem.noleggiaBicicletta(TipoBicicletta.ELETTRICA); });
+	}
+	
+	/* Questo test verifica che non vengano sollevate eccezioni nel caso si restituisca una bicicletta in una postazione con spazio libero */
+	@Test
+	public void restituzioneBiciclettaTest() {
+		Totem totem = new Totem("Via Dennissonis 66");
+		Bicicletta bici = new Bicicletta(TipoBicicletta.ELETTRICA);
+        Morsa morsa = new Morsa(TipoMorsa.ELETTRICA);
+        totem.aggiungiMorsa(morsa);
+        Assertions.assertDoesNotThrow(() -> { totem.restituisciBicicletta(bici); });
+	}
+	
+	/* Questo test verifica venga sollevata un'eccezione IllegalStateException nel caso si tenti di restituire una bicicletta in una postazione senza spazio libero */
+	@Test
+	public void restituzioneBiciclettaSenzaMorsaLiberaTest() {
+		Totem totem = new Totem("Via Dennissonis 66");
+		Bicicletta bici = new Bicicletta(TipoBicicletta.ELETTRICA);
+		Assertions.assertThrows(IllegalStateException.class, () -> { totem.restituisciBicicletta(bici); });
+	}
+	
+	@Test
+	public void numeroMorseOccupateTest() {
+		Totem totem = new Totem("Via Dennissonis 66");
+		Bicicletta bici = new Bicicletta(TipoBicicletta.ELETTRICA);
+        Morsa morsa = new Morsa(TipoMorsa.ELETTRICA);
+        morsa.chiudi(bici);
+        totem.aggiungiMorsa(morsa);
+        Assertions.assertEquals(totem.getNumeroMorseOccupate(), 1);
+	}
 }
